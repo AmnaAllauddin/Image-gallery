f4909a83b559e9ea29a509f2132cd8d11b3f3fe2
AmnaAllauddin <amnaallauddin2002@gmail.com>
2024-08-07 16:44:29 -0700

    updated

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const animal=document.getElementById('animals')
 const flower=document.getElementById('flowers')
 const seaview=document.getElementById('seaviews')
 const submit = document.getElementById('submit')
+const container = document.getElementById('background_container');
+const images = [
+    'url("beautiful-cloudscape.jpg")',
+    'url("26 Famous Landmarks in Canada.jpeg")',
+    'url("background3.jpg")'
+];
+let currentIndex = 0;
+
+function changeBackgroundImage() {
+   console.log(container.style.backgroundImage = images[currentIndex]) 
+    currentIndex = (currentIndex + 1) % images.length;
+}
+setInterval(changeBackgroundImage, 2000);
+changeBackgroundImage();
  tree.addEventListener('click',()=>{
     window.location.href="./tree.html"
 })
